4f09547f464f812b4df6b1f62a854ea5d6abb22e
Jack Rosenthal <jack@rosenth.al>
2026-04-11 03:18:50 +0000

    Implement clipboard set command

--- cmd/clipboard.go
+++ cmd/clipboard.go
@@ -1,9 +1,45 @@
 package cmd
 
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/jackrosenthal/plain-cli/internal/client"
+	"github.com/jackrosenthal/plain-cli/internal/output"
+)
+
+const setClipMutation = `mutation setClip($text: String!) {
+  setClip(text: $text)
+}`
+
 type ClipboardCmd struct {
 	Set ClipboardSetCmd `cmd:"" help:"Set the clipboard text."`
 }
 
 type ClipboardSetCmd struct {
 	Text string `arg:"" help:"Clipboard text."`
+}
+
+type clipboardMutationResponse struct {
+	Data struct {
+		SetClip bool `json:"setClip"`
+	} `json:"data"`
+}
+
+func (c *ClipboardSetCmd) Run(apiClient *client.Client, printer output.Printer) error {
+	var resp clipboardMutationResponse
+	if err := apiClient.GraphQL(context.Background(), setClipMutation, map[string]any{
+		"text": c.Text,
+	}, &resp); err != nil {
+		return fmt.Errorf("set clipboard: %w", err)
+	}
+	if !resp.Data.SetClip {
+		return errors.New("set clipboard: mutation returned false")
+	}
+
+	return printer.Print(mutationStatus{
+		Status:  "ok",
+		Message: "Updated device clipboard.",
+	})
 }
